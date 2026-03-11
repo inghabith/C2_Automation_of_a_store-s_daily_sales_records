b4ad5560b6edd812da70edc5455025eacf3e4055
kevinGon <kevinGonqgmail.com>
2026-03-11 07:24:33 -0500

    English translate and import functions in main branch

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-from script import registro_de_ventas
-registro_de_ventas
+from script import sales_record, total_results, calculator
+total_a = []
+products = []
+
+products = sales_record(products)
+
+total_result = total_results(products, total_a)
+
+total_a = total_result[1]
+products = total_result[0]
+
+calculator(total_a)
