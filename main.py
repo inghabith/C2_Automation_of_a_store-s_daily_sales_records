1703147c2d366c51d64e79fc9555551263e239f1
habith <ing.hdeleon1@gmail.com>
2026-03-12 16:06:45 -0500

    changes2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from script import sales_record, total_results, calculator
 total_a = []
 products = []
 
-products = sales_record(products)
+sales_record(products)
 
 total_result = total_results(products, total_a)
 
